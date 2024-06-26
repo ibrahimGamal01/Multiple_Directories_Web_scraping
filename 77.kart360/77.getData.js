0daef7784b16890fc7129510ddc61cf45cd7444c
Ibrahim Gamal <ibrahim.moustafa@ejust.edu.eg>
2024-06-26 10:32:04 +0300

    fix 77

--- 77.kart360/77.getData.js
+++ 77.kart360/77.getData.js
@@ -1,81 +1,3 @@
-// // const puppeteer = require('puppeteer');
-
-// // async function scrapeDetails(url) {
-// //     const browser = await puppeteer.launch();
-// //     const page = await browser.newPage();
-
-// //     try {
-// //         await page.goto(url);
-
-// //         // Wait for the necessary elements to ensure they are loaded
-// //         await page.waitForSelector('.u-text-xl');
-// //         await page.waitForSelector('.o-List-item');
-
-// //         // Extracting details
-// //         const details = await page.evaluate(() => {
-// //             const name = document.querySelector('.u-text-xl a') ? document.querySelector('.u-text-xl a').innerText : null;
-// //             const phone = document.querySelector('a[href^="tel:"]') ? document.querySelector('a[href^="tel:"]').innerText : null;
-// //             const address = document.querySelectorAll('.o-List-item .o-media__body')[1] ? document.querySelectorAll('.o-List-item .o-media__body')[1].innerText : null;
-// //             const imgURL = document.querySelector('.u-mr-4 a img') ? document.querySelector('.u-mr-4 a img').src : null;
-        
-// //             return { name, phone, address, imgURL };
-// //         });
-        
-
-// //         console.log(details);
-// //         return details;
-
-// //     } catch (error) {
-// //         console.error('Error during scraping:', error);
-// //         return {};
-// //     } finally {
-// //         await browser.close();
-// //     }
-// // }
-
-// // const url = 'https://kart360.com/tb-kart-usa';
-// // scrapeDetails(url);
-
-
-// const puppeteer = require('puppeteer');
-
-// async function scrapeDetails(url) {
-//     const browser = await puppeteer.launch();
-//     const page = await browser.newPage();
-
-//     try {
-//         await page.goto(url);
-
-//         // Wait for the necessary elements to ensure they are loaded
-//         await page.waitForSelector('.u-text-xl');
-//         await page.waitForSelector('.o-List-item');
-
-//         // Extracting details
-//         const details = await page.evaluate(() => {
-//             const name = document.querySelector('.u-text-xl a') ? document.querySelector('.u-text-xl a').innerText : null;
-//             const phone = document.querySelector('a[href^="tel:"]') ? document.querySelector('a[href^="tel:"]').innerText : null;
-//             const address = document.querySelectorAll('.o-List-item .o-media__body')[1] ? document.querySelectorAll('.o-List-item .o-media__body')[1].innerText : null;
-//             const imgURL = document.querySelector('.u-mr-4 a img') ? document.querySelector('.u-mr-4 a img').src : null;
-//             const website = document.querySelector('.o-List-item .o-media__body a.u-colorBase') ? document.querySelector('.o-List-item .o-media__body a.u-colorBase').href : null;
-
-//             return { name, phone, address, imgURL, website };
-//         });
-
-//         console.log(details);
-//         return details;
-
-//     } catch (error) {
-//         console.error('Error during scraping:', error);
-//         return {};
-//     } finally {
-//         await browser.close();
-//     }
-// }
-
-// const url = 'https://kart360.com/tb-kart-usa';
-// scrapeDetails(url);
-
-
 const puppeteer = require('puppeteer');
 const fs = require('fs');
 const path = require('path');
@@ -92,10 +14,16 @@ async function scrapeDetails(url) {
 
         const details = await page.evaluate(() => {
             const name = document.querySelector('.u-text-xl a') ? document.querySelector('.u-text-xl a').innerText.trim() : null;
-            const phone = document.querySelector('a[href^="tel:"]') ? document.querySelector('a[href^="tel:"]').innerText.trim() : null;
-            const address = document.querySelectorAll('.o-List-item .o-media__body')[1] ? document.querySelectorAll('.o-List-item .o-media__body')[1].innerText.trim() : null;
+            let phone = document.querySelector('a[href^="tel:"]') ? document.querySelector('a[href^="tel:"]').innerText.trim() : null;
+            let address = document.querySelectorAll('.o-List-item .o-media__body')[1] ? document.querySelectorAll('.o-List-item .o-media__body')[1].innerText.trim() : null;
             const imgURL = document.querySelector('.u-mr-4 a img') ? document.querySelector('.u-mr-4 a img').src : null;
             const website = document.querySelector('.o-List-item .o-media__body a.u-colorBase') ? document.querySelector('.o-List-item .o-media__body a.u-colorBase').href : null;
+
+            // If phone is null, use the first element (address)
+            if (phone == null) {
+                address = document.querySelectorAll('.o-List-item .o-media__body')[0] ? document.querySelectorAll('.o-List-item .o-media__body')[0].innerText.trim() : null;
+            }
+
             return { name, phone, address, imgURL, website };
         });
 
