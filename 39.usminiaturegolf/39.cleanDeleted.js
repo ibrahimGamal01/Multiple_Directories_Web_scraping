73aeb2107853dd001250c500aec1e710b17f1ad4
Ibrahim Gamal <ibrahim.moustafa@ejust.edu.eg>
2024-06-30 06:21:57 +0300

    Fix 39: reformart & merge all rows

--- 39.usminiaturegolf/39.cleanDeleted.js
+++ 39.usminiaturegolf/39.cleanDeleted.js
@@ -1,84 +1,31 @@
-// const fs = require('fs');
-
-// function processDeletedRecords(inputFilePath, outputFilePath) {
-//     fs.readFile(inputFilePath, 'utf8', (err, data) => {
-//         if (err) {
-//             console.error('Error reading the file:', err);
-//             return;
-//         }
-
-//         let jsonData;
-//         try {
-//             jsonData = JSON.parse(data);
-//         } catch (parseErr) {
-//             console.error('Error parsing JSON data:', parseErr);
-//             return;
-//         }
-
-//         const processedRecords = jsonData.map(item => {
-//             const name = item.name.replace(/\bMr\b/g, 'Mr').replace(/\bTom\b/g, 'Tom').replace(/\b&\b/g, '&');
-//             const phoneRegex = /(\d{3})-(\d{3})-(\d{4})/;
-//             const phoneMatch = item.phone.match(phoneRegex);
-//             const phone = phoneMatch ? `${phoneMatch[1]}-${phoneMatch[2]}-${phoneMatch[3]}` : '';
-//             return { name, phone };
-//         });
-
-//         fs.writeFile(outputFilePath, JSON.stringify(processedRecords, null, 2), (writeErr) => {
-//             if (writeErr) {
-//                 console.error('Error writing the processed file:', writeErr);
-//                 return;
-//             }
-//             console.log('Processed data has been written to', outputFilePath);
-//         });
-//     });
-// }
-
-// const inputFilePath = './39.Data.deleted.json';
-// const outputFilePath = './39.Data.processed.json';
-
-// processDeletedRecords(inputFilePath, outputFilePath);
-
-
 const fs = require('fs');
 
-function processDeletedRecords(inputFilePath, outputFilePath) {
-    fs.readFile(inputFilePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error('Error reading the file:', err);
-            return;
-        }
-
-        let jsonData;
-        try {
-            jsonData = JSON.parse(data);
-        } catch (parseErr) {
-            console.error('Error parsing JSON data:', parseErr);
-            return;
-        }
-
-        const processedRecords = jsonData.map(item => {
-            const phoneRegex = /(\d{3}-\d{3}-\d{4})/;
-            const phoneMatch = item.phone.match(phoneRegex);
-            let name = item.name;
-            let phone = '';
-            if (phoneMatch) {
-                phone = phoneMatch[1];
-                name += ' ' + item.phone.replace(phoneRegex, '').trim();
-            }
-            return { name, phone };
-        });
-
-        fs.writeFile(outputFilePath, JSON.stringify(processedRecords, null, 2), (writeErr) => {
-            if (writeErr) {
-                console.error('Error writing the processed file:', writeErr);
-                return;
-            }
-            console.log('Processed data has been written to', outputFilePath);
-        });
-    });
+function reformatEntry(entry) {
+    const phonePattern = /\b\d{3}[-.\s]??\d{3}[-.\s]??\d{4}\b/;
+    const phoneMatch = entry.phone.match(phonePattern);
+    
+    if (phoneMatch) {
+        const phone = phoneMatch[0];
+        const nameWithAddress = entry.phone.replace(phone, '').trim();
+        return {
+            name: `${entry.name}${nameWithAddress}`,
+            phone: phone
+        };
+    }
+    
+    return entry; // If no phone number found, return the original entry
 }
 
-const inputFilePath = './39.Data.deleted.json';
-const outputFilePath = './39.Data.processed.json';
+function reformatData(inputFile, outputFile) {
+    const data = JSON.parse(fs.readFileSync(inputFile, 'utf-8'));
 
-processDeletedRecords(inputFilePath, outputFilePath);
+    const reformattedData = data.map(reformatEntry);
+
+    fs.writeFileSync(outputFile, JSON.stringify(reformattedData, null, 2));
+    console.log(`Reformatted data has been written to ${outputFile}`);
+}
+
+const inputFilePath = '39.Data.deleted.json';
+const outputFilePath = 'reformatted_data.json';
+
+reformatData(inputFilePath, outputFilePath);
